39c5128c91438bca7ab9f5cb77cf508a722d7208
JieXin Liang <Alcanderian@users.noreply.github.com>
2023-10-16 20:06:44 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,22 +3,16 @@
 workdir=`pwd`
 
 if [ -z "$PPL_BUILD_THREAD_NUM" ]; then
-    if [[ `uname` == "Linux" ]]; then
-        PPL_BUILD_THREAD_NUM=`cat /proc/cpuinfo | grep processor | grep -v grep | wc -l`
-    elif [[ `uname` == "Darwin" ]]; then
-        PPL_BUILD_THREAD_NUM=`sysctl machdep.cpu | grep machdep.cpu.core_count | cut -d " " -f 2`
-    else
-        PPL_BUILD_THREAD_NUM=1
-    fi
-    echo -e "\e[1;33menv 'PPL_BUILD_THREAD_NUM' is not set. use PPL_BUILD_THREAD_NUM=${PPL_BUILD_THREAD_NUM} by default.\e[0m"
+    PPL_BUILD_THREAD_NUM=1
+    echo -e "env 'PPL_BUILD_THREAD_NUM' is not set. use PPL_BUILD_THREAD_NUM=${PPL_BUILD_THREAD_NUM} by default."
 fi
 
 build_type='Release'
-options="-DCMAKE_BUILD_TYPE=${build_type} -DCMAKE_INSTALL_PREFIX=install $*"
+options="$*"
 
 ppl_build_dir="${workdir}/ppl-build"
 mkdir ${ppl_build_dir}
 cd ${ppl_build_dir}
-cmd="cmake $options .. && cmake --build . -j ${PPL_BUILD_THREAD_NUM} --config ${build_type} && cmake --build . --target install -j ${PPL_BUILD_THREAD_NUM} --config ${build_type}"
+cmd="cmake $options .. && cmake --build . -j ${PPL_BUILD_THREAD_NUM} --config ${build_type}"
 echo "cmd -> $cmd"
 eval "$cmd"
