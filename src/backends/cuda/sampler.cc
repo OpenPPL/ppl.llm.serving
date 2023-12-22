1fc3150068c70b07e9e10d2501010495cc88aecf
alcanderian <alcanderian@gmail.com>
2023-12-22 17:02:49 +0800

    [fix] fix sampler

--- src/backends/cuda/sampler.cc
+++ src/backends/cuda/sampler.cc
@@ -78,7 +78,7 @@ RetCode Sampler::SampleTopPTopK(const float* logits_device, const float* tempera
 
     RetCode rc;
     if (top_p == 0.0) {
-        rc = ppl::kernel::llm::cuda::pmx::sample_argmax(stream_, logits_device, batch, vocab_size, cu_output_);
+        rc = ppl::kernel::llm::cuda::pmx::sample_argmax(stream_, logits_device, batch, vocab_size, vocab_size, cu_output_);
     } else {
         float* sorted_value = (float*)cu_output_;
         int32_t* sorted_index = (int32_t*)sorted_value + (int64_t)batch * pad_vocab_size;
@@ -90,8 +90,7 @@ RetCode Sampler::SampleTopPTopK(const float* logits_device, const float* tempera
             return RC_DEVICE_MEMORY_ERROR;
         }
 
-        // TODO: currently it has some bug
-        rc = ppl::kernel::llm::cuda::pmx::flash_sample_top_p(stream_, logits_device, batch, vocab_size,
+        rc = ppl::kernel::llm::cuda::pmx::flash_sample_top_p(stream_, logits_device, batch, vocab_size, vocab_size,
                                                              temperatures_device, top_p, sorted_value, sorted_index,
                                                              cu_output_ + output_offset);
     }
